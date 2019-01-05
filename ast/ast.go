aa658fd544ec73ba4d5dc13f44b147dd1dcb380e
kcaiueo <kcwebapply@gmail.com>
2019-01-05 20:00:51 +0900

    adapt returng

---
+++ ast/ast.go
@@ -0,0 +1,59 @@
+package ast
+
+import (
+	"github.com/kcwebapply/kc-interpreter/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// for Identifier
+type Identifier struct {
+	Token token.Token // token.IDENT トークン固定
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+// let statement
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// return value
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,48 +1,116 @@
 package parser
 
 import (
-	"github.com/kcwebapply/kc-interpreter/token"
+	"fmt"
+	"testing"
+
+	"github.com/kcwebapply/kc-interpreter/ast"
+	"github.com/kcwebapply/kc-interpreter/lexar"
 )
 
-type Node interface {
-	TokenLiteral() string
-}
+func TestLetStatements(t *testing.T) {
+	input := `
+  let x = 1+a;
+  let y = 5;
+  unko=8;
+  let foobar = 838383;
+  `
 
-type Statement interface {
-	Node
-	statementNode()
-}
+	l := lexar.New(input)
+	p := New(l)
 
-type Expression interface {
-	Node
-	expressionNode()
-}
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
-type Program struct {
-	Statements []Statement
-}
+	if program == nil {
+		t.Fatalf("parseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements, got=%d", len(program.Statements))
+	}
 
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatements(t, stmt, tt.expectedIdentifier) {
+			return
+		}
 	}
 }
 
-type LetStatement struct {
-	Token token.Token
-	Name  *Identifier
-	Value Expression
+// Test let statements parsing.
+func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not * ast.Letstatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not %s.got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("Token literal not %s,got %s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
 }
 
-func (ls *LetStatement) statementNode()       {}
-func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
-
-type Identifier struct {
-	Token token.Token // token.IDENT トークン固定
-	Value string
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d erros", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q, msg", msg)
+	}
+	t.FailNow()
 }
 
-func (i *Identifier) expressionNode()      {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+// Test Return statement parsing
+func TestReturnsStatement(t *testing.T) {
+	input := `
+  return 5;
+  return "ok";
+  return kc;
+  `
+
+	l := lexar.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements, got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("s not * ast.ReturnStatement. got=%T", returnStmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("s.TokenLiteral not 'let'. got=%q", returnStmt.TokenLiteral())
+		}
+
+		fmt.Println(returnStmt.ReturnValue)
+
+	}
+
+}
